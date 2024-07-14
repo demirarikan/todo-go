5234f1800bd5100d2ca907d0246d1daf10386b3a
Demir Arikan <dmrarikan@gmail.com>
2024-07-13 22:43:22 -0400

    initial todo app implementation

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2024 Demir Arikan dmrarikan@gmail.com
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "todo-go",
+	Short: "todo-go is a todo application",
+	Long: `A simple todo application to help you get done more in less time. 
+	it is designed to be simple and to help the user accomplish their goals.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todo-go.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ todo/todo.go
@@ -0,0 +1,6 @@
+package todo
+
+
+type Item stuct {
+	Text string
+}
